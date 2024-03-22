3bedcbcfb9c879d6fab68be3971d4cd2ebf9341a
Nikolay M. <vindetto4004@gmail.com>
2024-03-22 22:25:20 +0300

    the_big_changes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import telebot
 import parser
+import const
 from telebot import types
 from additions import *
 
@@ -7,45 +9,63 @@ token = get_token(param_name='TOKEN')
 bot = launch_bot(token=token)
 
 # Главное меню
-main_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-btn1 = types.KeyboardButton('Мой план')
-btn2 = types.KeyboardButton('Профиль')
-btn3 = types.KeyboardButton('Напоминалки')
-main_menu_markup.row(btn1)
-main_menu_markup.row(btn2, btn3)
+main_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton(const.edit_plan_text)
+btn2 = types.KeyboardButton(const.profile_text)
+btn3 = types.KeyboardButton(const.reminders_text)
+main_markup.row(btn1)
+main_markup.row(btn2, btn3)
 
 # Меню редактирования плана
-plan_menu_markup = None  # P.S: ДОДЕЛАТЬ!
+plan_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton(const.edit_plan_text)
+btn2 = types.KeyboardButton(const.back_main)
+plan_markup.row(btn1)
+plan_markup.row(btn2)
 
 # Меню профиля
-profile_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-btn1 = types.KeyboardButton('Статистика')
-btn2 = types.KeyboardButton('Настройки')
-btn3 = types.KeyboardButton('Назад')
-profile_menu_markup.row(btn1, btn2)
-profile_menu_markup.row(btn3)
+profile_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton(const.statistics_text)
+btn2 = types.KeyboardButton(const.settings_text)
+btn3 = types.KeyboardButton(const.back_main)
+profile_markup.row(btn1, btn2)
+profile_markup.row(btn3)
 
 # Меню напоминалок
-reminder_menu_markup = None  # P.S: ДОДЕЛАТЬ!
+reminder_markup = None  # P.S: ДОДЕЛАТЬ!
+
+# Меню начала новой попытки
+start_attempt_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton(const.start_attempt_text)
+start_attempt_markup.row(btn1)
+
+# Кнопка продолжить
+continue_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn1 = types.KeyboardButton(const.continue_text)
+continue_markup.row(btn1)
+
+
 
 def main_menu(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, '', reply_markup=main_menu_markup)
+    bot.send_message(chat_id, '', reply_markup=main_markup)
 
 
 def my_plan(message):
     chat_id = message.chat.id
+    print(chat_id)
 
-    try:
-        user_id = parser.get_user_id_of_chat(chat_id)
-        attempt_id = parser.get_current_attempt(user_id).id
-        day = parser.get_current_day_of_attempt(attempt_id)
-        tasks = parser.get_tasks_of_day(day.id)
+    user_id = parser.get_user_id_of_chat(chat_id)
+    attempt = parser.get_current_attempt(user_id)
+    if attempt is None:
+        return bot.send_message(chat_id, 'Ошибка: Вероятно, Вам необходимо начать новую попытку!', reply_markup=start_attempt_markup)
 
-    except:
-        bot.send_message(chat_id, 'Ошибка: Вероятно Вам необходимо начать новую попытку!', reply_markup=main_menu_markup)
+    day = parser.get_current_day_of_attempt(attempt.attempt_id)
+    tasks = parser.get_tasks_of_day(day.day_id)
 
-    mess = f'План на день №{day.relative_pos}\n'
+
+
+    mess = f'План на день №{day.relative_pos}\n\n'
 
     if tasks.shape[0] == 0:
         mess += '- Вы пока не определили задачи на этот день'
@@ -55,42 +75,293 @@ def my_plan(message):
         for row_tuple in tasks.iterrows():
             task = row_tuple[1]
 
-            sign = 'ДА' if task.is_completed else 'НЕТ'
+            sign = '✅' if task.is_completed else '❌'
 
-            mess += f'{sign} - {task.task_name}'
+            mess += f'{sign} - {task.task_name}\n'
 
-    mess += f'\nВыполнено: {tasks.is_completed.sum()}/{tasks.shape[1]}'
+        mess += f'\nВыполнено: {tasks.is_completed.sum()}/{tasks.shape[0]}'
 
-    bot.send_message(chat_id, mess, reply_markup=plan_menu_markup)
+    bot.send_message(chat_id, mess, reply_markup=main_markup)
+
+
+def edit_plan(message) -> None:
+    chat_id = message.chat.id
+
+    user_id = parser.get_user_id_of_chat(chat_id)
+    attempt_id = parser.get_current_attempt(user_id).attempt_id
+    day_id = parser.get_current_day_of_attempt(attempt_id).day_id
+    tasks = parser.get_tasks_of_day(day_id).sort_values('is_completed', ascending=False)
+
+    edit_menu_markup = types.InlineKeyboardMarkup()
+    for row_tuple in tasks.iterrows():
+        task = row_tuple[1]
+        sign = '✅' if task.is_completed else '❌'
+        text = f'{sign} - {task.task_name}'
+        button = types.InlineKeyboardButton(text, callback_data=f'view_task_{task.task_id}')
+        edit_menu_markup.row(button)
+
+    add_text = '+' if tasks.shape[0] else 'Добавить задачу'
+
+
+    btn1 = types.InlineKeyboardButton(add_text, callback_data='add_task')
+    btn2 = types.InlineKeyboardButton('Удалить все задачи', callback_data='delete_all_tasks')
+    btn3 = types.InlineKeyboardButton(const.back_main, callback_data='go_back')
+
+    if tasks.shape[0] < 9:
+        edit_menu_markup.row(btn1)
+
+    if tasks.shape[0]:
+        edit_menu_markup.row(btn2)
+
+    edit_menu_markup.row(btn3)
+
+    mess = 'Редактор задач'
+
+    bot.send_message(chat_id, mess, reply_markup=edit_menu_markup)
+
+
+def view_task(message, task_id: int) -> None:
+    chat_id = message.chat.id
+    task = parser.get_task(task_id)
+
+    task_name = task.task_name
+
+    mess = f'Задача: {task_name}\n'
+    mess += f'Статус: {'✅' if task.is_completed == 1 else '❌'}'
+
+    if task.is_completed == 1:
+        btn1 = types.InlineKeyboardButton('❌ Убрать выполнение', callback_data=f'mark_complete_{task_id}')
+
+    else:
+        btn1 = types.InlineKeyboardButton('✅ Выполнено', callback_data=f'mark_complete_{task_id}')
+
+    btn2 = types.InlineKeyboardButton('Изменить текст задачи', callback_data=f'EditText_task_{task_id}')
+    btn3 = types.InlineKeyboardButton('🗑 Удалить задачу', callback_data=f'delete_task_{task_id}')
+    btn4 = types.InlineKeyboardButton(const.back_main, callback_data='go_back_edit')
+
+    edit_task_markup = types.InlineKeyboardMarkup()
+    edit_task_markup.row(btn1)
+    edit_task_markup.row(btn2, btn3)
+    edit_task_markup.row(btn4)
+
+    bot.edit_message_text(mess, chat_id, message.message_id, reply_markup=edit_task_markup)
+
+def add_task(message) -> None:
+    @bot.message_handler(content_types=['text'])
+    def message_input_step(message):
+        chat_id = message.chat.id
+        if message.text == const.back_edit:
+            return general_message_handler(message)
+
+        bot.delete_messages(message.chat.id, [message.message_id, message.message_id - 1])
+        task_name = message.text
+
+        markup = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(const.back_main, callback_data='go_back_add_task')
+        markup.row(button)
+
+        if len(task_name) <= 64:
+            user_id = parser.get_user_id_of_chat(message.chat.id)
+            attempt_id = parser.get_current_attempt(user_id).attempt_id
+            day_id = parser.get_current_day_of_attempt(attempt_id).day_id
+            tasks = parser.get_tasks_of_day(day_id)
+
+            if tasks[tasks.task_name == task_name].shape[0] == 0:
+                data = [[day_id, task_name, 0]]
+                parser.sqlwrapper.append_data('tasks', pd.DataFrame(data, columns=['day_id', 'task_name', 'is_completed']))
+
+                #markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                #button = types.KeyboardButton(const.back_edit)
+                #markup.row(button)
+
+                bot.delete_messages(chat_id, [message.message_id, message.message_id - 1])
+                edit_plan(message)
+
+            else:
+                mess = 'Ошибка: Задача с таким именем уже существует!'
+                bot.send_message(chat_id, mess, reply_markup=markup)
+
+        else:
+            mess = 'Ошибка: Слишком длинное название для задачи'
+            bot.send_message(chat_id, mess, reply_markup=markup)
+
+    chat_id = message.chat.id
+
+    menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton(const.back_edit)
+    menu_markup.row(button)
+
+    mess = 'Опишите задачу: '
+
+    msg = bot.send_message(chat_id, mess, reply_markup=menu_markup)
+    bot.register_next_step_handler(msg, message_input_step)
+
+def my_profile(message) -> None:
+    chat_id = message.chat.id
+    ...
+
+def start_attempt(message) -> None:
+    """Начинает новую попытку для пользователя"""
+    chat_id = message.chat.id
+    user_id = parser.get_user_id_of_chat(chat_id)
+
+    current_attempt = parser.get_current_attempt(user_id)
+    if current_attempt is not None:  # если попытка имеется
+        are_you_sure_markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton('Да', callback_data='start_new_attempt')
+        btn2 = types.InlineKeyboardButton('Нет', callback_data='go_back')
+        are_you_sure_markup.row(btn1, btn2)
+
+        current_day = parser.get_current_day_of_attempt(current_attempt.attempt_id)
+        mess = 'У вас уже есть текущая попытка!\n'
+        mess += f'Кол-во пройденных дней: {current_day.relative_pos}\n'
+        mess += 'Вы уверены, что хотите начать сначала?'
+
+        bot.send_message(chat_id, mess, reply_markup=are_you_sure_markup)
+
+
+    else:  # Начинаем новую попытку
+        parser.start_new_attempt_of_user(user_id)
+
+        if parser.get_current_attempt(user_id) is not None:  # операция прошла успешно
+            mess = 'Вы начали новую попытку\n'
+            mess += 'Это означает, что ...\n'
+            mess += 'Что-ж, самое время составить план на сегодняшний день!'
+            bot.send_message(chat_id, mess, reply_markup=continue_markup)
+
+        else:  # случилась какая-то ошибка
+            bot.send_message(chat_id, 'Произошла какая-то ошибка. Попробуйте ещё раз')
+
+def delete_all_tasks(message) -> None:
+    chat_id = message.chat.id
+
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Да', callback_data='yes_delete_all')
+    btn2 = types.InlineKeyboardButton('Нет', callback_data='go_back_edit')
+    markup.row(btn1, btn2)
+
+    msg = bot.edit_message_text('Вы уверены, что хотите удалить ВСЕ задачи?', chat_id, message.message_id, reply_markup=markup)
+
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def start(message) -> None:
     chat_id = message.chat.id
-    is_chat_exists = False  # нужно проверить, общались ли ранее
-    if not is_chat_exists:
+
+    if not parser.get_user_id_of_chat(chat_id): # Пользователь не зарегистрирован
         mess = f"Привет, {message.from_user.first_name}! Вижу тебя впервые...\n"
         mess += "Я создан для того, чтобы помогать людям быть более продуктивными!\n"
         mess += "Здесь ты можешь составлять план на день, а я буду напоминать тебе о важных задачах и поддерживать на всём пути развития!\n"
         mess += "Итак, ты готов научиться чему-то новому, развить полезную привычку и в целом, стать лучше? Я готов стать твоим спутником.\n\n\n"
-        bot.send_message(chat_id, mess, reply_markup=main_menu_markup)
-        # + Создай запись пользователя в БД
+        bot.send_message(chat_id, mess, reply_markup=continue_markup)
+        # Регистрация пользователя в БД
+        parser.sqlwrapper.append_data('users', pd.DataFrame([[chat_id]], columns=['chat_id']))
+
+    else:
+        general_message_handler(message)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback) -> None:
+    chat_id = callback.message.chat.id
+    user_id = parser.get_user_id_of_chat(chat_id)
+
+    if callback.data == 'start_new_attempt':  # Начать новую попытку (нажал "да")
+        current_attempt = parser.get_current_attempt(user_id)
+        parser.finish_the_attempt(current_attempt)
+        start_attempt(callback.message)
+
+    elif callback.data == 'go_back':  # Кнопка "назад"
+        msg_id = callback.message.message_id
+        bot.delete_message(chat_id, msg_id)
+        my_plan(callback.message)
+
+    elif callback.data == 'go_back_edit':
+        msg_id = callback.message.message_id
+        bot.delete_message(chat_id, msg_id)
+        edit_plan(callback.message)
+
+    elif callback.data == 'go_back_add_task':
+        msg_id = callback.message.message_id
+        bot.delete_message(chat_id, msg_id)
+        add_task(callback.message)
+
+    elif 'view_task_' in callback.data:  # Просмотр конкретной задачи
+        task_id = int(callback.data.split('_')[2])
+        view_task(callback.message, task_id)
+
+    elif 'delete_task_' in callback.data:  # Удаление конкретной задачи
+        task_id = int(callback.data.split('_')[2])
+        parser.delete_task(task_id)
+        bot.delete_message(chat_id, callback.message.message_id)
+        edit_plan(callback.message)
+
+    elif 'mark_complete_' in callback.data:  # Изменить статус выполнения задачи
+        task_id = int(callback.data.split('_')[2])
+        task = parser.get_task(task_id)
+        parser.set_status_task(task_id, abs(task.is_completed - 1))
+        view_task(callback.message, task_id)
+
+    elif callback.data == 'add_task':  # Добавление новой задачи
+        bot.delete_message(chat_id, callback.message.message_id)
+        add_task(callback.message)
+
+    elif callback.data == 'delete_all_tasks':  # Удалить сразу все задачи
+        delete_all_tasks(callback.message)
+
+    elif callback.data == 'yes_delete_all':
+        chat_id = callback.message.chat.id
+        user_id = parser.get_user_id_of_chat(chat_id)
+        attempt_id = parser.get_current_attempt(user_id).attempt_id
+        day_id = parser.get_current_day_of_attempt(attempt_id).day_id
+        tasks = parser.get_tasks_of_day(day_id)
+
+        for i, task in tasks.iterrows():
+            parser.delete_task(task.task_id)
+
+        markup = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(const.back_edit, callback_data='go_back_edit')
+        markup.row(button)
+
+        bot.delete_message(chat_id, callback.message.message_id)
+        bot.send_message(chat_id, 'Все задачи были успешно удалены!', reply_markup=markup)
+
+    else:  # для дебагинга (можешь удалить при деплое)
+        bot.send_message(chat_id, 'Я не понял callback (функция: callback_message())')
+
+
+@bot.message_handler(commands=['test'])
+def test(message):
+    """Функция для тестов ( ПРИ ДЕПЛОЕ УДАЛИТЬ! )"""
+    msg = bot.send_message(message.chat.id, 'some text', reply_markup=plan_markup)
 
 
 @bot.message_handler(content_types=['text'])
 def general_message_handler(message):
     chat_id = message.chat.id
-    delete_previos_messages(message, bot, 2)
+    #delete_previos_messages(message, bot, 2)
+    bot.delete_messages(chat_id, [message.message_id, message.message_id - 1, message.message_id - 2])
 
-    if message.text == 'Мой план':
+    if message.text == const.edit_plan_text:  # редактировать план
+        edit_plan(message) # редактирование плана
+
+    elif message.text == const.profile_text:  # мой профиль
+        my_profile(message)
+
+    elif message.text == const.back_main:  # назад в главное меню
         my_plan(message)
 
-    elif message.text == ...:
-        ...
+    elif message.text == const.back_edit:  # назад в меню редактирования
+        edit_plan(message)
+
+    elif message.text == const.start_attempt_text:  # начать новую попытку
+        start_attempt(message)
+
+    elif message.text == const.continue_text:  # продолжить
+        my_plan(message)
 
     else:
-        bot.send_message(chat_id, 'Я тебя не понял', reply_markup=main_menu_markup)
-
+        bot.send_message(chat_id, 'Ошибка: Вы ввели неизвестную команду', reply_markup=continue_markup)
 
 
 print(Fore.RED + '*' + Style.RESET_ALL + ' Бот работает')
-bot.polling(non_stop=True)  # P.S: С Богом! ;)
+bot.polling(none_stop=True)  # P.S: С Богом! ;)
--- parser.py
+++ parser.py
@@ -1,11 +1,13 @@
+import numpy as np
 import pandas as pd
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session
 import json
+import datetime as dt
 
 class SqlWrapper:
     def __init__(self, config_file: str):
         """При инициализации сразу настраивает подключение к БД"""
-
         with open(config_file, 'r') as json_file:
             db_config = json.load(json_file)
 
@@ -13,10 +15,10 @@ class SqlWrapper:
         db_url = db_url.format(**db_config)
 
         self.engine = sqlalchemy.create_engine(db_url, echo=False)
+        self.Session = scoped_session(sessionmaker(bind=self.engine))
 
-    def read_data(self, table_name: str):
+    def read_data(self, table_name: str) -> pd.DataFrame:
         """Считывает данные таблицы в DataFrame"""
-
         with self.engine.begin() as conn:
             df = pd.read_sql_table(
                 table_name=table_name,
@@ -25,39 +27,109 @@ class SqlWrapper:
 
         return df
 
-    def append_data(self, table_name, dataframe: pd.DataFrame):
+    def append_data(self, table_name, dataframe: pd.DataFrame | pd.Series):
         """Добавляет данные из DataFrame в таблицу"""
-
         with self.engine.begin() as conn:
             dataframe.to_sql(name=table_name, if_exists='append', con=conn, index=False)
 
+    def update_data(self, table_name, dataframe: pd.DataFrame | pd.Series):
+        """Обновляет существующие данные таблицы"""
+        with self.engine.begin() as conn:
+            dataframe.to_sql(name=table_name, if_exists='replace', con=conn)
+
+    def execute(self, sql):
+        query = sqlalchemy.text(sql)
+
+        with self.engine.connect() as conn:
+            result = conn.execute(query)
+            conn.commit()
+
+            return result
 
 def get_user_id_of_chat(chat_id: int) -> int:
+    """Получает chat_id, а затем выдаёт user_id соответствующего пользователя"""
     df = sqlwrapper.read_data('users')
-    user_id = df[df.chat_id == chat_id].iloc[0].user_id
+    df = df[df.chat_id == chat_id]
 
-    return user_id
+    if df.shape[0]:
+        return df.iloc[0].user_id
+
 
 def get_current_attempt(user_id: int) -> pd.Series:
+    """Возвращает текущую попытку пользвателя <user_id>
+    Если её нет - None."""
     df = sqlwrapper.read_data('attempts')
     attempt = df[(df.user_id == user_id)&(df.isna().date_end)]
 
-    if attempt.shape[1] == 0:
-        raise Exception('У пользователя нет текущей попытки!')
-
-    return attempt
+    return attempt.iloc[0] if attempt.shape[0] else None
 
 def get_current_day_of_attempt(attempt_id: int) -> pd.Series:
+    """Возвращает текущий день попытки или None, если его нет"""
     df = sqlwrapper.read_data('days')
-    day = df[df.attempt_id == attempt_id].sort_values('relative_pos', ascending=False).iloc[0]
+    df = df[df.attempt_id == attempt_id].sort_values('relative_pos', ascending=False)
 
-    return day
+    return df.iloc[0] if df.shape[0] else None
 
 def get_tasks_of_day(day_id: int) -> pd.Series:
+    """Возвращает список задач конкретного дня"""
     df = sqlwrapper.read_data('tasks')
     tasks = df[df.day_id == day_id]
 
     return tasks
+
+def get_task(task_id: int) -> pd.Series | None:
+    """Возвращает задачу с id = task_id. Если таковой нет, то None"""
+    tasks = sqlwrapper.read_data('tasks')
+    task = tasks[tasks.task_id == task_id]
+
+    return task.iloc[0] if task.shape[0] else None
+
+def delete_task(task_id: int) -> None:
+    """Удаляет задачу с id = task_id (* Должна существовать !)"""
+    sql = f"DELETE FROM tasks WHERE task_id = {task_id}"
+    sqlwrapper.execute(sql)
+
+def set_status_task(task_id: int, new_value: int) -> None:
+    """Обновляет статус выполнения задачи на new_value"""
+    sql = f"UPDATE tasks SET is_completed = {new_value} WHERE task_id = {task_id}"
+    sqlwrapper.execute(sql)
+
+def finish_the_attempt(attempt: pd.Series) -> None:
+    """Завершает попытку.
+    * Если попытка уже является завершённой, то ничего не делает
+    * Удаляет исходную запись попытки, если она была начата и удалена в один и тот же день"""
+    attempt_id = int(attempt.attempt_id)
+
+    if attempt.isna().date_end:
+        date_end = dt.datetime.now().date().isoformat()
+        date_beg = attempt.date_beg.to_pydatetime().date().isoformat()
+
+        if date_end == date_beg:
+            sql = f"DELETE FROM attempts WHERE attempt_id = {attempt_id}"
+
+        else:
+            sql = f"UPDATE attempts SET date_end = '{date_end}' WHERE attempt_id = {attempt_id}"
+
+        sqlwrapper.execute(sql)
+
+def start_new_attempt_of_user(user_id: int) -> None:
+    """Начинает новую попытку для пользователя
+    * Примечание: Во избежание ошибок, у пользователя не должно быть текущей попытки"""
+    # Создаём новую попытку
+    data = [[user_id, dt.date.today().isoformat(), np.NaN]]
+    sqlwrapper.append_data('attempts', pd.DataFrame(data, columns=['user_id', 'date_beg', 'date_end']))
+
+    # Начинаем первый день
+    attempt_id = get_current_attempt(user_id).attempt_id
+    start_new_day_of_attempt(attempt_id)
+
+def start_new_day_of_attempt(attempt_id: int) -> None:
+    """Создаёт запись нового дня для конкретной попытки"""
+    last_day = get_current_day_of_attempt(attempt_id)
+    relative_pos = last_day.relative_pos + 1 if last_day else 1
+    data = [[attempt_id, relative_pos, np.NaN, np.NaN]]
+
+    sqlwrapper.append_data('days', pd.DataFrame(data, columns=['attempt_id', 'relative_pos', 'total_tasks', 'completed_tasks']))
 
 
 sqlwrapper = SqlWrapper('mysql_config.json')
